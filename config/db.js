3855e827e879d42541afeb4db181e6ef65495dfa
lidiamarserl <141480899+lidiamarserl@users.noreply.github.com>
2025-03-19 11:48:16 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,30 +1,8 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const mongoUri = process.env.MONGO_URI ||  'mongodb+srv://WMSGHCI:GH123@wmsghci.mlyez.mongodb.net/warehouse-ghci?retryWrites=true&w=majority&appName=WMSGHCI'; 
-
-// const dbConnect  = async () => {
-//     try {
-//         await mongoose.connect(mongoUri, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log("MongoDB connection SUCCESS (warehouse-ghci)");
-//     } catch (error) {
-//         console.error(`MongoDB Connection Error: ${error.message}`);
-//         process.exit(1);
-//     }
-//     };
-
-// export default dbConnect;
-
 const mongoose = require('mongoose');
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://WMSGHCI:GH123@wmsghci.mlyez.mongodb.net/warehouse-ghci?retryWrites=true&w=majority&appName=WMSGHCI', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
